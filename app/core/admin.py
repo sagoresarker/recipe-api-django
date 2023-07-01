590b760009edc3ca91041a78bd35970785d961db
sagoresarker <sagoresarker.cse@gmail.com>
2023-07-01 19:10:24 +0600

    added ingredient model, ingredient api and also updated the recipe api

--- app/core/admin.py
+++ app/core/admin.py
@@ -51,6 +51,7 @@ class UserAdmin(BaseUserAdmin):
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Recipe)
 admin.site.register(models.Tag)
+admin.site.register(models.Ingredient)
 
 
 
