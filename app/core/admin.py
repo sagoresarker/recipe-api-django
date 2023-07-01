37cd3db12e11c6a3257492f9e1a89d8d90267677
sagoresarker <sagoresarker.cse@gmail.com>
2023-07-01 10:42:26 +0600

    implement tag model, tags api, update recipe endpoints.

--- app/core/admin.py
+++ app/core/admin.py
@@ -50,6 +50,7 @@ class UserAdmin(BaseUserAdmin):
 
 admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Recipe)
+admin.site.register(models.Tag)
 
 
 
--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -8,6 +8,12 @@ from django.contrib.auth import get_user_model
 
 from core import models
 
+
+def create_user(email='user@example.com', password='testpass123'):
+    """Create and return a new user. """
+    return get_user_model().objects.create_user(email, password)
+
+
 class ModelTest(TestCase):
     """test models"""
 
@@ -67,3 +73,11 @@ class ModelTest(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
+
+    def test_create_tag(self):
+        """Test creating a tag is succesful. """
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name='Tag1')
+
+        self.assertEqual(str(tag), tag.name)
